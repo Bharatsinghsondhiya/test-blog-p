2ace81a3f8409e4d8affcac474d1973019134441
bharat singh <bharatsinghsondhiya4@gmail.com>
2025-11-24 16:27:33 +0530

    services auth added and commited

---
+++ services/auth.js
@@ -0,0 +1,27 @@
+const JWT = require("jsonwebtoken")
+const secret = "hello@secret09"
+
+//creating token for user
+function createTokenForUser(user){
+    const payload = {
+        _id: user._id,
+        email: user.email,
+         profileImageURL:user. profileImageURL,
+         role: user.role,
+
+    };
+    const token = JWT.sign(payload,secret);
+    return token;
+}
+
+
+function validateToken(token){
+    const payload = JWT.verify()
+(token,secret)
+    return payload;
+}
+
+module.exports = {
+    createTokenForUser,
+    validateToken,
+}
